623212396ac18cc70f3da118478a063f546d1202
divbish <divya.bishnoi@gmail.com>
2017-10-08 10:11:41 -0400

    initial commit

---
+++ practice8/src/practice8/Practice8.java
@@ -0,0 +1,13 @@
+package practice8;
+
+public class Practice8 {
+
+	public static void main(String[] args) {
+		String[] arr = {"vikram","bishnoi"};
+		for(String a: arr){
+			System.out.printf("%s\n", a);
+		}
+
+	}
+
+}
